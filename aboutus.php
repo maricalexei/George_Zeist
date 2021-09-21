bc6289c7a48aec66356b4d84e055c233067ffd00
AronVLD <327156@student.mboutrecht.nl>
2021-09-21 16:07:11 +0200

    Changed AboutUs

--- aboutus.php
+++ aboutus.php
@@ -4,27 +4,39 @@
             <div class="card-body">
                 <h5 class="card-title">Contact</h5>
                 <p class="card-text">
-                    Phone Number <br>
-                    Email
-                
+                    Hier vind u alle relevante informatie<br>
+                    over het restaurant George.<br>
+                    Als u meer vragen heeft, Wees<br>
+                    niet bang om ons een Email te<br>
+                    sturen of ons te bellen.
                 </p>
-
                 <p class="card-text"><small class="text-muted"></small></p>
             </div>
         </div>
-
     </div>
     <div class="col-4">
         <div class="card">
             <div class="card-body">
                 <h5 class="card-title">Contact</h5>
-                <p class="card-text"></p>
+                <p class="card-text">
+                    Phone Number: 061234567 <br>
+                    Email: contact@george-kanaleneiland.nl<br>
+                    <br>
+                    Working Hours:<br>
+                    dinsdag: 11:00–01:00<br>
+                    woensdag: 11:00–01:00<br>
+                    donderdag: 11:00–01:00<br>
+                    vrijdag: 11:00–01:00<br>
+                    zaterdag: 11:00–01:00<br>
+                    zondag: 11:00–01:00<br>
+                    maandag: 11:00–01:00<br>
+                </p>
+
                 <p class="card-text"><small class="text-muted"></small></p>
             </div>
         </div>
-
     </div>
     <div class="col-4">
-        <img src="" alt="..." class="img-fluid">
+        <div class="mapouter"><div class="gmap_canvas"><iframe width="400" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=Slotlaan%20280%2C%203701%20GW%20Zeist&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://vincheckeurope.eu">VIN Check</a><br><style>.mapouter{position:relative;text-align:right;height:400px;width:400px;}</style><a href="https://embedmaps.info">Embed Google Maps</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:600px;width:600px;}</style></div></div>
     </div>
 </div>
