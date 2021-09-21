d581f0237b4f207ca980e931e87fe1c9576a15f3
AronVLD <327156@student.mboutrecht.nl>
2021-09-21 15:42:41 +0200

    AboutUs

---
+++ aboutus.php
@@ -0,0 +1,30 @@
+<div class="row px-0">
+    <div class="col-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Contact</h5>
+                <p class="card-text">
+                    Phone Number <br>
+                    Email
+                
+                </p>
+
+                <p class="card-text"><small class="text-muted"></small></p>
+            </div>
+        </div>
+
+    </div>
+    <div class="col-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Contact</h5>
+                <p class="card-text"></p>
+                <p class="card-text"><small class="text-muted"></small></p>
+            </div>
+        </div>
+
+    </div>
+    <div class="col-4">
+        <img src="" alt="..." class="img-fluid">
+    </div>
+</div>
