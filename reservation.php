18347d3acc3867ba5351a355c31ec956ace21462
AronVLD <327156@student.mboutrecht.nl>
2021-10-05 09:42:32 +0200

    added reservation.php

---
+++ reservation.php
@@ -0,0 +1,90 @@
+<div class="section">
+    <div class="container">
+        <div class="content-section">
+            <div class="title">
+                <h1>Reservation</h1>
+                <hr>
+            </div>
+            <!-- <div class="content">
+                <label for="exampleDataList" class="form-label">Choose Your Table</label>
+                <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
+                <datalist id="datalistOptions">
+                    <option value="1">
+                    <option value="2">
+                    <option value="3">
+                    <option value="4">
+                    <option value="5">
+                    <option value="6">
+                    <option value="7">
+                    <option value="8">
+                    <option value="9">
+                    <option value="10">
+                    <option value="11">
+                    <option value="12">
+                    <option value="13">
+                    <option value="14">
+                    <option value="15">
+                    <option value="16">
+                    <option value="17">
+                    <option value="18">
+                    <option value="19">
+                    <option value="20">
+                    <option value="21">
+                    <option value="22">
+                    <option value="t1">
+                    <option value="t2">
+                    <option value="t3">
+                    <option value="t4">
+                    <option value="t5">
+                    <option value="t6">
+                    <option value="t7">
+                    <option value="t8">
+                    <option value="Bar">
+                </datalist>
+            </div> -->
+            <div class="mb-3">
+                <label for="disabledSelect" class="form-label">Disabled select menu</label>
+                <select id="disabledSelect" class="form-select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                    <option>11</option>
+                    <option>12</option>
+                    <option>13</option>
+                    <option>14</option>
+                    <option>15</option>
+                    <option>16</option>
+                    <option>17</option>
+                    <option>18</option>
+                    <option>19</option>
+                    <option>20</option>
+                    <option>21</option>
+                    <option>22</option>
+                    <option>t1</option>
+                    <option>t2</option>
+                    <option>t3</option>
+                    <option>t4</option>
+                    <option>t5</option>
+                    <option>t6</option>
+                    <option>t7</option>
+                    <option>t8</option>
+                    <option>Bar</option>
+                </select>
+            </div>
+            <div>
+                <button>click om te reserveren</button>
+            </div>
+
+        </div>
+        <div class="image-section">
+            <img src="img\reservation.jpg">
+        </div>
+    </div>
+</div>
