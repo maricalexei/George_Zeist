db8da2224cb4ee6809ee384c467422691f6046e0
AronVLD <327156@student.mboutrecht.nl>
2021-10-05 10:05:02 +0200

    added extra input field

--- reservation.php
+++ reservation.php
@@ -5,43 +5,6 @@
                 <h1>Reservation</h1>
                 <hr>
             </div>
-            <!-- <div class="content">
-                <label for="exampleDataList" class="form-label">Choose Your Table</label>
-                <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
-                <datalist id="datalistOptions">
-                    <option value="1">
-                    <option value="2">
-                    <option value="3">
-                    <option value="4">
-                    <option value="5">
-                    <option value="6">
-                    <option value="7">
-                    <option value="8">
-                    <option value="9">
-                    <option value="10">
-                    <option value="11">
-                    <option value="12">
-                    <option value="13">
-                    <option value="14">
-                    <option value="15">
-                    <option value="16">
-                    <option value="17">
-                    <option value="18">
-                    <option value="19">
-                    <option value="20">
-                    <option value="21">
-                    <option value="22">
-                    <option value="t1">
-                    <option value="t2">
-                    <option value="t3">
-                    <option value="t4">
-                    <option value="t5">
-                    <option value="t6">
-                    <option value="t7">
-                    <option value="t8">
-                    <option value="Bar">
-                </datalist>
-            </div> -->
             <div class="mb-3">
                 <label for="disabledSelect" class="form-label">Disabled select menu</label>
                 <select id="disabledSelect" class="form-select">
@@ -77,9 +40,13 @@
                     <option>t8</option>
                     <option>Bar</option>
                 </select>
+                <div class="mb-3">
+                    <label for="FormControlTextarea1" class="form-label">Requests</label>
+                    <textarea class="form-control" id="FormControlTextarea1" rows="3"></textarea>
+                </div>
             </div>
             <div>
-                <button>click om te reserveren</button>
+                <button type="button" class="btn btn-primary">Send</button>
             </div>
 
         </div>
