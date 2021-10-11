aec10f3e432e433fabdc27d754eb3fa45d02b435
QEUN <332275@student.mboutrecht.nl>
2021-10-11 15:20:41 +0200

    login page
    added login page

---
+++ login.php
@@ -0,0 +1,22 @@
+<div class="container mt-5">
+    <div class = "row">
+        <div class="col-12 col-sm-6">
+            <form>
+                <div class=mb-3>
+                <label for input="InputEmail" class="form-label">Email</label>
+                <input name="Email" type="Email" class="form-control" id="InputEmail"></input>
+
+                </div>
+
+
+            </form>
+
+        </div>
+
+
+
+    </div>
+
+
+
+</div>
