8fb5b60abe758728cdf9c12c2f3efdb179b7395d
QEUN <332275@student.mboutrecht.nl>
2021-10-11 15:25:28 +0200

    added the input fields for Email and Password

--- login.php
+++ login.php
@@ -3,20 +3,14 @@
         <div class="col-12 col-sm-6">
             <form>
                 <div class=mb-3>
-                <label for input="InputEmail" class="form-label">Email</label>
+                <label for input="InputEmail" class="form-label">Email<br></label>
                 <input name="Email" type="Email" class="form-control" id="InputEmail"></input>
-
                 </div>
-
-
+                <div class=mb-3>
+                <label for input="InputPassword" class="form-label">Password<br></label>
+                <input name="Password" type="Password" class="form-control" id="InputPassword"></input>
+                </div>
             </form>
-
         </div>
-
-
-
     </div>
-
-
-
 </div>
