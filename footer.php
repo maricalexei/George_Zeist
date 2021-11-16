f8b575541f39f03045080ba65a1672898505c6a7
Maric-Alexei <329525@student.mboutrecht.nl>
2021-11-16 12:25:51 +0100

    Change: added a new page called career, Type: addition, Why: so people can orient themself about the availble jobs

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 <div class="container footer_bar" style="padding-top: 15px;">
-    <div class="row">
-        <div class="col-4">Copyright George Zeist</div>
-        <div class="col-4">Terms & Privacy</div>
-        <div class="col-4">Made by group George Zeist</div>
-    </div>
+    <section>
+        <div class="row">
+            <div class="col-4">Copyright George Zeist</div>
+            <div class="col-4">Terms & Privacy</div>
+            <div class="col-4">Made by group George Zeist</div>
+        </div>
+    </section>
 </div>
