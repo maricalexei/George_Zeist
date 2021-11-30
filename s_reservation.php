0933aed312c7b43c39ef514bd5ca5e299795bb45
Maric-Alexei <329525@student.mboutrecht.nl>
2021-11-30 12:55:09 +0100

    Removeds all the full calander files and made my own PHP calender

--- s_reservation.php
+++ s_reservation.php
@@ -1,5 +1,18 @@
-<script src='node_modules/fullcalendar/main.js'></script>
-
+<?php
+include ("./functions.php");
+include ("./connect_db.php")
+?>
 <div class="container">
-    <div id='calendar'></div>
+    <div class="row">
+        <div class="col-md-12">
+            <div id="calendar">
+                <?php
+                $dateComponents = getdate();
+                $month = $dateComponents['mon'];
+                $year = $dateComponents['year'];
+                echo build_calender($month,$year);
+                ?>
+            </div>
+        </div>
+    </div>
 </div>
