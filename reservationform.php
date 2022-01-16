e5c34b84ed4e5e1cc75d4aa4353591df1c49ab87
AronVLD <327156@student.mboutrecht.nl>
2022-01-16 15:09:49 +0100

    Reservation email+form

---
+++ reservationform.php
@@ -0,0 +1,32 @@
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-12" style="text-align: center;">
+            <h1>How was the food?</h1>
+            <br>
+            <br>
+            <div class="mb-3">
+                <!-- email -->
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <!-- fill in -->
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">How was the food?</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <!-- star rate -->
+            <div class="rate">
+                <input type="radio" id="star5" name="rate" value="5" />
+                <label for="star5" title="text">5 stars</label>
+                <input type="radio" id="star4" name="rate" value="4" />
+                <label for="star4" title="text">4 stars</label>
+                <input type="radio" id="star3" name="rate" value="3" />
+                <label for="star3" title="text">3 stars</label>
+                <input type="radio" id="star2" name="rate" value="2" />
+                <label for="star2" title="text">2 stars</label>
+                <input type="radio" id="star1" name="rate" value="1" />
+                <label for="star1" title="text">1 star</label>
+            </div>
+        </div>
+    </div>
+</div>
