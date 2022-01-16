be6939ebd1286a4970e94bea9f175be27b43657a
QEUN <332275@student.mboutrecht.nl>
2022-01-16 22:47:24 +0100

    added info specific to student

--- resultstudent.php
+++ resultstudent.php
@@ -1,17 +1,50 @@
 <?php
-
 //include the connect for the db.
 include("./connect_db.php");
 include("./functions.php");
 
+$student=$_SESSION['email'];
 //sellection for the db
-$slt = $conn->query("SELECT `afkorting` FROM `medewerker`");
-$plt = $conn->query("SELECT `id` FROM `restaurantinschrijving`");
+$sql=$conn->query("SELECT * FROM `student` WHERE `email` = $student");
 
 
 
 ?>
 <body>
+    <h3>Personel information</h3>
+    <table style="width: 80%" border="1">
+        <tr>
+            <th>studentnr</th>
+            <th>voornaam</th>
+            <th>tussenvoegsel</th>
+            <th>achternaam</th>
+            <th>mobiel</th>
+            <th>woonplaats</th>
+            <th>straat</th>
+            <th>postcode</th>
+        </tr>
+        <?php
+        $qryi="SELECT * FROM `student` WHERE `email`='$student'";
+        $runi=$conn->query($qryi);
+        if($runi->num_rows>0){
+            while($rowi=$runi->fetch_assoc()){
+                $studentnr=$rowi['studentnr'];
+        ?>
+        <tr>
+            <td><?php echo $rowi['studentnr']?></td>
+            <td><?php echo $rowi['voornaam']?></td>
+            <td><?php echo $rowi['tussenvoegsel']?></td>
+            <td><?php echo $rowi['achternaam']?></td>
+            <td><?php echo $rowi['mobiel']?></td>
+            <td><?php echo $rowi['woonplaats']?></td>
+            <td><?php echo $rowi['straat']?></td>
+            <td><?php echo $rowi['postcode']?></td>
+        </tr>
+        <?php
+            }
+        }
+        ?>
+
     <h3>Results List</h3>
     <table style="width: 80%" border="1">
         <tr>
